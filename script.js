504df22b82aebad832ecf97b650294d4e6ff17b4
Sille Hvedehave <sillehvedehave@silles-macbook-pro.home>
2020-03-25 12:52:14 +0100

    rettelser

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-$('.carousel').carousel({
-    interval: 4000
-})
-
+//$('.carousel').carousel({
+//    interval: 4000
+//})
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+$(window).on("load", function (e) {
+    $('.preloader').fadeOut('slow');
+})
+
+//
+//    console.log("pre");
