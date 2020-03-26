96ef63b349b2115e88be3deba8c741575b3ab800
Sille Hvedehave <sillehvedehave@silles-macbook-pro.home>
2020-03-26 08:55:06 +0100

    scroll

--- script.js
+++ script.js
@@ -11,3 +11,38 @@ $(window).on("load", function (e) {
 
 //
 //    console.log("pre");
+
+
+ScrollReveal().reveal('#om', {
+    delay: 900
+});
+ScrollReveal().reveal('#produkter', {
+    delay: 500
+});
+ScrollReveal().reveal('#produkter h3', {
+    delay: 500
+});
+ScrollReveal().reveal('#produkter p', {
+    delay: 500
+});
+ScrollReveal().reveal('#materiale', {
+    delay: 500
+});
+ScrollReveal().reveal('.nyhed1', {
+    delay: 500
+});
+ScrollReveal().reveal('.nyhed2', {
+    delay: 500
+});
+ScrollReveal().reveal('.nyhed3', {
+    delay: 500
+});
+ScrollReveal().reveal('#materiale', {
+    delay: 500
+});
+ScrollReveal().reveal('#ingredienser', {
+    delay: 500
+});
+ScrollReveal().reveal('#kontakt', {
+    delay: 500
+});
